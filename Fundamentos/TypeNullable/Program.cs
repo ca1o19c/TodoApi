ffce293e3be6a09a395532c812e4d87e6d2ecd67
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-24 17:43:10 -0300

    Type nullable

---
+++ Fundamentos/TypeNullable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TypeNullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? age = 0;
+            Console.WriteLine(age);
+
+            age = null;
+            Console.WriteLine(age);
+
+            age = 25;
+            Console.WriteLine(age);
+        }
+    }
+}
