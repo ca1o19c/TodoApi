a77da8e4a15b1c4df6a77d1c82e6a8fefcbcf668
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-24 14:08:43 -0300

    Tipos primitivos

---
+++ Fundamentos/TiposPrimitivos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TiposPrimitivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte meuByte = 1;
+            int meuInt = 50;
+            short meuShort = 6;
+            long meuLong = 1809;
+        }
+    }
+}
