2222b23db0ff91df8e1c70ea443c82b61e520782
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-24 14:34:37 -0300

    Tipos nulos

---
+++ Fundamentos/TiposNulos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TiposNulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? idade = 0;
+            Console.WriteLine(idade);
+
+            idade = null;
+            Console.WriteLine(idade);
+
+            idade = 25;
+            Console.WriteLine(idade);
+        }
+    }
+}
