bfefb5213c55c4b8e8d3dfdaf26223a761e913e3
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-24 14:47:28 -0300

    Convertendo tipos

---
+++ Fundamentos/ConvertendoTipos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConvertendoTipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro = 100;
+            float real = 25.5f;
+
+            // real = inteiro;
+            // inteiro = (int) real;
+
+            // String valorReal = real.ToString();
+            
+            inteiro = Convert.ToInt32(real);
+            
+            Console.WriteLine(inteiro);
+        }
+    }
+}
