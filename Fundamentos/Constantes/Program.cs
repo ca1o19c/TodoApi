cda702173e304245470466145d0a31b0bd34d7ad
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-24 13:46:13 -0300

    Variaveis

---
+++ Fundamentos/Constantes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IDADE_MINIMA = 18;
+            Console.WriteLine(IDADE_MINIMA);
+        }
+    }
+}
