8345a1091ca71c7ec636defafbbf086609ad7d3a
Caio Antonio <caio.antonio.c@outlook.com>
2021-10-24 13:38:04 -0300

    Variaveis

---
+++ Fundamentos/Variaveis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            idade = 19;
+            var idade = 25;
+
+            var texto = "Textando";
+            Console.WriteLine(texto);
+        }
+    }
+}
