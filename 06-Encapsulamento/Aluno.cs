49bd1c4737b9e7ff44b31d32abfe3ca6d1574d60
Caio Roberto <caio.roberto@sensedia.com>
2021-11-09 11:05:27 -0300

    Encapsulamento

---
+++ 06-Encapsulamento/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06_Encapsulamento
+{
+    class Aluno
+    {
+        private double nota1, nota2;
+
+        private double Media()
+        {
+            return (nota1 + nota2) / 2;
+        }
+
+        public void Mensagem()
+        {
+            Console.Write("Entre com a primeira nota: ");
+            nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Entre com a segunda nota: ");
+            nota2 = Convert.ToInt32(Console.ReadLine());
+
+            double value = Media();
+            Console.WriteLine("Você está com a média " + value);
+        }
+    }
+}
---
+++ 06-Encapsulamento/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _06_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Aluno();
+
+            a.Mensagem();
+        }
+    }
+}
