f1482e78d509e2dc2e40a491ed20b6df9034810b
Caio Roberto <caio.roberto@sensedia.com>
2021-11-09 13:12:10 -0300

    This

---
+++ 09-This/Anime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _09_This
+{
+    class Anime
+    {
+        private string nome = "Dragon Ball";
+
+        public Anime(string nome)
+        {
+            Console.WriteLine(nome);
+            Console.WriteLine(this.nome);
+        }
+    }
+}
---
+++ 09-This/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09_This
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Anime("Pokemon");
+        }
+    }
+}
