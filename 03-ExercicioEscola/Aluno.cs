5b8906b481d28eb211b3b175ac17c4e4f9efbe77
Caio Roberto <caio.roberto@sensedia.com>
2021-11-09 10:08:20 -0300

    Exercício escola

---
+++ 03-ExercicioEscola/Aluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03_ExercicioEscola
+{
+    class Aluno
+    {
+        // Atributos
+        public string nome;
+        public double nota1, nota2;
+
+        // Métodos
+
+        // Média
+        public double Media()
+        {
+            return (nota1 + nota2) / 2;
+        }
+
+        // Situação
+        public string Situacao(double media)
+        {
+            return (media > 7) ? "Aprovado" : "reprovado";
+        }
+
+        // Mensagem
+        public void Mensagem()
+        {
+            double obterMedia = Media();
+
+            string obterSituacao = Situacao(obterMedia);
+
+            Console.WriteLine(nome + " sua situação é " + obterSituacao + " com média " + obterMedia);
+        }
+    }
+}
---
+++ 03-ExercicioEscola/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03_ExercicioEscola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            a.nome = "Caio";
+            a.nota1 = 8.9;
+            a.nota2 = 9.7;
+
+            a.Mensagem();
+        }
+    }
+}
