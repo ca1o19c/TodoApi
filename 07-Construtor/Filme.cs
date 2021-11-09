6af3763968e315f74a1e05d3ac7c296d5da3b5a2
Caio Roberto <caio.roberto@sensedia.com>
2021-11-09 11:10:07 -0300

    Construtores

---
+++ 07-Construtor/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07_Construtor
+{
+    class Filme
+    {
+        public Filme()
+        {
+            Console.WriteLine("Construtor executado");
+        }
+
+        public Filme(string titulo)
+        {
+            Console.WriteLine("Construtor com parametro! o valor é " + titulo);
+        }
+    }
+}
---
+++ 07-Construtor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _07_Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Filme();
+            var f2 = new Filme("Homem Aranha");
+        }
+    }
+}
