a877c85e73c9997750236b070a4bf44c67f54614
Caio Roberto <caio.roberto@sensedia.com>
2021-11-09 13:01:57 -0300

    Getters and Setters

---
+++ 08-GetSet/Carro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_GetSet
+{
+    class Carro
+    {
+        private string nome;
+        private int ano;
+
+        public string Nome 
+        {
+            get { return nome; }
+            set { nome = value;  } 
+        }
+
+        public int Ano
+        {
+            get { return ano; }
+            set { ano = value; }
+        }
+    }
+}
---
+++ 08-GetSet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08_GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Carro();
+
+            c1.Nome = "Fiat Mobi";
+            c1.Ano = 2022;
+
+            Console.Write(c1.Nome + " ");
+            Console.WriteLine(c1.Ano);
+        }
+    }
+}
