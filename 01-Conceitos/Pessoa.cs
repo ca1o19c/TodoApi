f002878b7e60460c2093193a71927207c007fa3c
Caio Roberto <caio.roberto@sensedia.com>
2021-11-09 09:40:29 -0300

    Classes, atributos, métodos e objetos

---
+++ 01-Conceitos/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Conceitos
+{
+    class Pessoa
+    {
+        // Atributos
+        public string nome;
+        public int idade;
+
+        // Metódos
+        public void Mensagem()
+        {
+            Console.WriteLine(this.nome + " tem " + this.idade + " anos");
+        }
+    }
+}
--- 01-Conceitos/Program.cs
+++ 01-Conceitos/Program.cs
@@ -5,14 +5,13 @@ namespace _01_Conceitos
     class Program
     {
         static void Main(string[] args)
-        {   
-            // Instanciar um objeto
+        {
             Pessoa p = new Pessoa();
 
             p.nome = "Caio";
             p.idade = 19;
 
-            p.mensagem();
+            p.Mensagem();
         }
     }
 }
