15dff8728b9b92a7049b715ec444b5004d885a2e
Caio Roberto <caio.roberto@sensedia.com>
2021-11-09 10:54:50 -0300

    Modificadores de Acesso

---
+++ 05-ModificadoresAcesso/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05_ModificadoresAcesso
+{
+    class Produto
+    {
+        /*
+         public      ->    Atributos e métodos visíveis em qualquer classe
+         private     ->    Atributos e métodos visíveis apenas na classe onde são criados
+         protected   ->    Atributos e métodos visíveis em classes onde são criados ou herdados
+        */
+
+        public string nome;
+        private double valor;
+    }
+}
---
+++ 05-ModificadoresAcesso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05_ModificadoresAcesso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Produto();
+        }
+    }
+}
