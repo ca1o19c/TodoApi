d84631851821e4ca27b54c4f57fba9dbcdf8f56f
Caio Roberto <caio.roberto@sensedia.com>
2021-11-11 09:15:29 -0300

    Atributos e métodos estáticos

---
+++ 12-AtributosMetodosEstaticos/Exemplo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _12_AtributosMetodosEstaticos
+{
+    class Exemplo
+    {
+        public static string nome = "Caio";
+
+        public static void Soma(int n1, int n2)
+        {
+            Console.WriteLine(n1 + n2);
+        }
+    }
+}
---
+++ 12-AtributosMetodosEstaticos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _12_AtributosMetodosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo.Soma(7, 8);
+
+            Console.WriteLine("Olá " + Exemplo.nome);
+        }
+    }
+}
