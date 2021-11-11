c7bd60ea4ca055050215cc1f42fb11924ea0b445
Caio Roberto <caio.roberto@sensedia.com>
2021-11-11 09:53:31 -0300

    Interface

---
+++ 14-Interface/IPadrao.cs
@@ -0,0 +1,9 @@
+﻿namespace _14_Interface
+{
+    interface IPadrao
+    {
+        void Somar(int n1, int n2);
+
+        void Subtrair(int n1, int n2);
+    }
+}
---
+++ 14-Interface/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _14_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Calculo();
+
+            c.Somar(9, 9);
+
+            c.Subtrair(10, 1);
+        }
+    }
+}
